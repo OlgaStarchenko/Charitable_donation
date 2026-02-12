cf4d465a603455ee089348182147bfeb33d669b5
OlgaStarchenko <o9915530@gmail.com>
2026-02-12 10:10:08 +1000

    Initializing the Form Component

--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,20 @@
-import { Component } from '../core/Component';
+import { Component } from "../core/Component";
 
 export class List extends Component {
   setup() {
-    this.$rootElement = document.createElement('div');
-    this.$rootElement.className = 'donates-container';
+    this.$rootElement = document.createElement("div");
+    this.$rootElement.className = "donates-container";
 
-    // ...
+    const donatesContainerTtitle = document.createElement("h2");
+    donatesContainerTtitle.className = "donates-container__title";
+    donatesContainerTtitle.textContent = "Список донатов";
+
+    const donatesContainer = document.createElement("div");
+    donatesContainer.className = "donates-container__donates";
+
+    this.$listContainer = donatesContainer;
+    this.$rootElement.appendChild(donatesContainerTtitle);
+    this.$rootElement.appendChild(donatesContainer);
   }
 
   addItem(item) {
